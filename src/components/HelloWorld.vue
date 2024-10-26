1120a60d79334069a2da7996982a1bed11043f4c
tangfq <941867223@qq.com>
2024-10-26 18:25:24 +0800

    项目初化并获取合约的账户功能

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,51 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+<script setup>
+import { ref,onMounted } from "vue";
+import Web3 from "web3";
+import mtContract from '../contracts/MyTokenDemo.json'
+
+//1.web3js
+const web3 = new Web3(
+  Web3.givenProvider || 'wss://sepolia.infura.io/ws/v3/018c25a8b5ce4f23810c49f60478478f');
+
+console.log('web3', Web3.version);
+
+let contract = new web3.eth.Contract(mtContract.abi, '0x6e401C817Df452C96f384Cae6A096F622b38C989');
+console.log(contract);
+
+const getCoinInfo = async()=>{
+  try {
+    // 请求用户授权调用metamask
+    await window.ethereum.enable();
+    const account = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    console.log('account', account);
+  } catch (error) {
+    // 如果用户拒绝授权
+    console.error("User denied account access");
   }
+  const blance = await contract.methods.balanceOf('0x6e401C817Df452C96f384Cae6A096F622b38C989').call();
+  console.log('blance', blance);
+  
 }
+
+
+
+
+const msg = ref("hello1");
+
+onMounted(()=>{
+  getCoinInfo();
+});
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -2,3 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+if(typeof window.ethereum !=='undefined'){
+    console.log('MetaMask is installed!');
+}
